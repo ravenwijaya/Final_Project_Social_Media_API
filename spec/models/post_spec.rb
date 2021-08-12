3c0cf89469711c046fe0c459d749cffd2919c899
ravenwijaya <wijayaraven@gmail.com>
2021-08-12 15:37:13 +0700

    add Test for Post Model #initialize and #valid? when initialized without user_id
    Signed-off-by: ravenwijaya <wijayaraven@gmail.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,5 +14,16 @@ describe Post do
                 expect(post.valid?).to eq(true)
             end
         end
+        context 'when initialized without user_id' do
+            it 'should return false' do
+                params={
+                    'id' => 1,
+                    'content' => 'What are the most popular Instagram #hashtags by category?',
+                    'file_path' => '/mnt/c/Users/wijay/code/final/public/uploads/81Y4tT_iJhaYBI-LnwvYowfat5uc.jpg'
+                }
+                post = Post.new(params)
+                expect(post.valid?).to eq(false)
+            end
+        end
     end
 end
