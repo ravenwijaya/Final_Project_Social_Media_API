a46a5ca9c24986f30f7d84ee472abc44eed877c6
ravenwijaya <wijayaraven@gmail.com>
2021-08-12 15:25:24 +0700

    add Test for Tag Model #initialize,valid,exist when when initialized with registered tag
    Signed-off-by: ravenwijaya <wijayaraven@gmail.com>

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -14,5 +14,17 @@ describe Tag do
                 expect(tag.valid?).to eq(true)
             end
         end
+        context 'when initialized with registered tag' do
+            it 'should return false' do
+                params={
+                    'name' => '#hashtag'
+                }
+                tag = Tag.new(params)
+                mock_client = double
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+                allow(mock_client).to receive(:query).with("select id from tags where name = '#{tag.name}'").and_return([{"id"=>1}])
+                expect(tag.valid?).to eq(false)
+            end
+        end
     end
 end
