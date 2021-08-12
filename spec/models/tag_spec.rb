bb3d0ca8eb0b6ab47d1cddbb0c54018916365cf6
ravenwijaya <wijayaraven@gmail.com>
2021-08-12 15:26:19 +0700

    add Test for Tag Model #save when save tag
    Signed-off-by: ravenwijaya <wijayaraven@gmail.com>

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -27,4 +27,22 @@ describe Tag do
             end
         end
     end
+    describe '#save' do
+        context "when save tag" do
+            it 'should return id' do
+                params={
+                    'name' => '#hashtag'
+                }
+                convert_result=[Tag.new({'id' => 1,'name'=>'#hashtag'})]
+                tag = Tag.new(params)
+                mock_client = double
+                allow(tag).to receive(:valid?).and_return(true)
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+                expect(mock_client).to receive(:query).with("insert into tags(name) values ('#{tag.name}')")
+                expect(mock_client).to receive(:query).with("select id from tags where name ='#{tag.name}'")
+                allow(Tag).to receive(:convert_sql_result_to_array).and_return(convert_result)
+                expect(tag.save).to eq(1)  
+            end
+        end
+    end
 end
