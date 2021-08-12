c8da96dd678af5fb5c996fa38c26d6dbb5312adf
ravenwijaya <wijayaraven@gmail.com>
2021-08-12 15:13:03 +0700

    add Test for User Model when initialized with registered email or username
    Signed-off-by: ravenwijaya <wijayaraven@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,20 @@ describe User do
                 expect(user.valid?).to eq(true)
             end
         end
+        context 'when initialized with registered email or username' do
+            it 'should return false' do
+                params={
+                    'username' => 'raven',
+                    'email' => 'raven@gmail.com',
+                    'bio' => 'bio'
+                }
+                user = User.new(params)
+                mock_client = double
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+                allow(mock_client).to receive(:query).with("select id from users where email = '#{user.email}' or username = '#{user.username}'").and_return([{'id' => 1}])
+                expect(user.valid?).to eq(false)
+            end
+        end
     end
     
 end
