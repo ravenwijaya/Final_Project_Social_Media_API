f66beb5a273b0c9895cf038cf4523930516f5a93
ravenwijaya <wijayaraven@gmail.com>
2021-08-12 15:38:34 +0700

    add Test for Post Model #as_json when as json
    Signed-off-by: ravenwijaya <wijayaraven@gmail.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -37,4 +37,23 @@ describe Post do
             end
         end
     end
+    describe '#as_json' do
+        context "when as json" do
+            it 'should return as json' do
+                post = Post.new({
+                    'id' => 1,
+                    'user_id' => 1,
+                    'content' => 'What are the most popular Instagram #hashtags by category?',
+                    'file_path' => '/mnt/c/Users/wijay/code/final/public/uploads/81Y4tT_iJhaYBI-LnwvYowfat5uc.jpg'
+            })
+                expected_result = {
+                    id: post.id, 
+                    content: post.content, 
+                    user_id: post.user_id, 
+                    file_path: post.file_path, 
+                    tags: ['#hashtags']}
+                expect(post.as_json).to eq(expected_result)
+            end
+        end
+    end
 end
