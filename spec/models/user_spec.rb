61611684d33838a47b24ee01af91c47481b9444e
ravenwijaya <wijayaraven@gmail.com>
2021-08-12 15:14:52 +0700

    add Test for User Model when initialized without bio
    Signed-off-by: ravenwijaya <wijayaraven@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,19 @@ describe User do
                 expect(user.valid?).to eq(false)
             end
         end
+        context 'when initialized without bio' do
+            it 'should return true' do
+                params={
+                    'username' => 'raven',
+                    'email' => 'raven@gmail.com'
+                }
+                user = User.new(params)
+                mock_client = double
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+                allow(mock_client).to receive(:query).with("select id from users where email = '#{user.email}' or username = '#{user.username}'").and_return([])
+                expect(user.valid?).to eq(true)
+            end
+        end
     end
     
 end
